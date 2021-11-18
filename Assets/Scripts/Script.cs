cbc5d7f18e17282825b9a984fcb37082fed3b987
Chion0phile <Chion0phile@users.noreply.github.com>
2021-11-18 21:07:49 +0100

    Initial commit

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Nie interere bo kici kici kociej mordy dostaniesz
+}
